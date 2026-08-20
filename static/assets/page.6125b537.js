9c39f4914bf40d831049bc5efa1c4b5d69f6da40
DiegoMcDowell <130522570+DiegoMcDowell@users.noreply.github.com>
2026-08-19 21:57:28 -0600

    navbar black

--- static/assets/page.6125b537.js
+++
@@ -1,3 +0,0 @@
-import{C as s,m as e}from"./index.b6be5691.js";window.CTFd=s;window.Alpine=e;e.start();async function d(){return(await(await fetch("/api/v1/scoreboard")).json()).data}function c(o){const a=document.getElementById("scoreboard-widget"),r=o.map(t=>`
-        <li>${t.pos}. ${t.name} \u2014 ${t.score} pts</li>
-    `).join("");a.innerHTML=`<ul>${r}</ul>`}async function n(){const o=await d();c(o)}n();setInterval(n,1e4);
---
+++ static/assets/page.ad3021d7.js
@@ -0,0 +1,3 @@
+import{C as s,m as e}from"./index.ffab3559.js";window.CTFd=s;window.Alpine=e;e.start();async function d(){return(await(await fetch("/api/v1/scoreboard")).json()).data}function c(o){const a=document.getElementById("scoreboard-widget"),r=o.map(t=>`
+        <li>${t.pos}. ${t.name} \u2014 ${t.score} pts</li>
+    `).join("");a.innerHTML=`<ul>${r}</ul>`}async function n(){const o=await d();c(o)}n();setInterval(n,1e4);
