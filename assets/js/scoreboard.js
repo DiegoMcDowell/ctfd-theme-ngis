57504fa77be6c2631c0763ce0dae75ae10d113b7
DiegoMcDowell <130522570+DiegoMcDowell@users.noreply.github.com>
2026-08-26 21:17:47 -0600

    Scoreboard ten teams

--- assets/js/scoreboard.js
+++ assets/js/scoreboard.js
@@ -1,4 +1,5 @@
 import Alpine from "alpinejs";
+import * as echarts from "echarts/core";
 import CTFd from "./index";
 import { getOption } from "./utils/graphs/echarts/scoreboard";
 import { embed } from "./utils/graphs/echarts";
@@ -13,19 +14,68 @@ Alpine.data("ScoreboardDetail", () => ({
   data: {},
   show: true,
   activeBracket: null,
+  range: null, // hours to display, null means show everything
 
-  async update() {
-    this.data = await CTFd.pages.scoreboard.getScoreboardDetail(10, this.activeBracket);
-
+  render() {
     let optionMerge = window.scoreboardChartOptions;
     let option = getOption(CTFd.config.userMode, this.data, optionMerge);
 
+    if (this.range) {
+      let maxTime = null;
+      for (const team of Object.values(this.data)) {
+        for (const solve of team.solves) {
+          const time = new Date(solve.date).getTime();
+          if (maxTime === null || time > maxTime) {
+            maxTime = time;
+          }
+        }
+      }
+
+      if (maxTime !== null) {
+        option.xAxis[0].min = maxTime - this.range * 60 * 60 * 1000;
+        option.xAxis[0].max = maxTime;
+      }
+    }
+
     embed(this.$refs.scoregraph, option);
+  },
+
+  setRange(hours) {
+    this.range = hours;
+    this.render();
+  },
+
+  downloadImage() {
+    const chart = echarts.getInstanceByDom(this.$refs.scoregraph);
+    if (!chart) return;
+
+    const link = document.createElement("a");
+    link.href = chart.getDataURL({ pixelRatio: 2, backgroundColor: "#0c0d0e" });
+    link.download = "scoreboard.png";
+    link.click();
+  },
+
+  toggleFullscreen() {
+    const panel = this.$refs.scoregraphpanel;
+    if (!document.fullscreenElement) {
+      panel.requestFullscreen?.();
+    } else {
+      document.exitFullscreen?.();
+    }
+  },
+
+  async update() {
+    this.data = await CTFd.pages.scoreboard.getScoreboardDetail(10, this.activeBracket);
+    this.render();
     this.show = Object.keys(this.data).length > 0;
   },
 
   async init() {
     this.update();
+
+    document.addEventListener("fullscreenchange", () => {
+      echarts.getInstanceByDom(this.$refs.scoregraph)?.resize();
+    });
 
     setInterval(() => {
       this.update();
--- assets/js/utils/graphs/echarts/scoreboard.js
+++ assets/js/utils/graphs/echarts/scoreboard.js
@@ -3,34 +3,51 @@ import { mergeObjects } from "../../objects";
 import { cumulativeSum } from "../../math";
 import dayjs from "dayjs";
 
+const AXIS_LINE_COLOR = "rgba(255, 255, 255, 0.15)";
+const AXIS_LABEL_COLOR = "rgba(255, 255, 255, 0.5)";
+const SPLIT_LINE_COLOR = "rgba(255, 255, 255, 0.08)";
+
 export function getOption(mode, places, optionMerge) {
   let option = {
+    backgroundColor: "transparent",
     title: {
-      left: "center",
+      show: false,
       text: "Top 10 " + (mode === "teams" ? "Teams" : "Users"),
     },
     tooltip: {
       trigger: "axis",
       axisPointer: {
         type: "cross",
+        label: {
+          color: "#fff",
+        },
+      },
+      backgroundColor: "#0c0d0e",
+      borderColor: "rgba(229, 9, 20, 0.5)",
+      textStyle: {
+        color: "#fff",
       },
     },
     legend: {
       type: "scroll",
       orient: "horizontal",
       align: "left",
-      bottom: 35,
+      bottom: 0,
+      icon: "circle",
+      itemWidth: 10,
+      itemHeight: 10,
+      pageIconColor: "#fff",
+      pageTextStyle: {
+        color: AXIS_LABEL_COLOR,
+      },
       data: [],
     },
     toolbox: {
-      feature: {
-        dataZoom: {
-          yAxisIndex: "none",
-        },
-        saveAsImage: {},
-      },
+      show: false,
     },
     grid: {
+      top: 20,
+      bottom: 55,
       containLabel: true,
     },
     xAxis: [
@@ -38,22 +55,34 @@ export function getOption(mode, places, optionMerge) {
         type: "time",
         boundaryGap: false,
         data: [],
+        axisLine: {
+          lineStyle: {
+            color: AXIS_LINE_COLOR,
+          },
+        },
+        axisLabel: {
+          color: AXIS_LABEL_COLOR,
+        },
+        splitLine: {
+          show: false,
+        },
       },
     ],
     yAxis: [
       {
         type: "value",
-      },
-    ],
-    dataZoom: [
-      {
-        id: "dataZoomX",
-        type: "slider",
-        xAxisIndex: [0],
-        filterMode: "filter",
-        height: 20,
-        top: 35,
-        fillerColor: "rgba(233, 236, 241, 0.4)",
+        axisLine: {
+          show: false,
+        },
+        axisLabel: {
+          color: AXIS_LABEL_COLOR,
+        },
+        splitLine: {
+          lineStyle: {
+            color: SPLIT_LINE_COLOR,
+            type: "dashed",
+          },
+        },
       },
     ],
     series: [],
@@ -74,20 +103,32 @@ export function getOption(mode, places, optionMerge) {
       return [e, total_scores[i]];
     });
 
-    option.legend.data.push(places[teams[i]]["name"]);
+    const color = colorHash(places[teams[i]]["name"] + places[teams[i]]["id"]);
+
+    option.legend.data.push({
+      name: places[teams[i]]["name"],
+      itemStyle: {
+        color: color,
+      },
+      textStyle: {
+        color: color,
+      },
+    });
 
     const data = {
       name: places[teams[i]]["name"],
       type: "line",
-      label: {
-        normal: {
-          position: "top",
-        },
+      smooth: true,
+      symbolSize: 6,
+      showSymbol: true,
+      lineStyle: {
+        width: 2,
+        color: color,
+        shadowBlur: 8,
+        shadowColor: color,
       },
       itemStyle: {
-        normal: {
-          color: colorHash(places[teams[i]]["name"] + places[teams[i]]["id"]),
-        },
+        color: color,
       },
       data: scores,
     };
